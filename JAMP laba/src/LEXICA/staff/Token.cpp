562812500b149b8e8eca2c96e8b1356762732b87
waterofox <a3shirnin@gmail.com>
2025-10-06 16:33:55 +0300

    easy token

--- JAMP laba/src/LEXICA/staff/Token.cpp
+++ JAMP laba/src/LEXICA/staff/Token.cpp
@@ -1,1 +1,17 @@
 #include "Token.h"
+
+Token::Token(const token_type& type, const std::string& lexema)
+{
+	this->type = type;
+	this->lexema = lexema;
+}
+
+const Token::token_type& Token::get_type()
+{
+	return this->type;
+}
+
+const std::string& Token::get_lexema()
+{
+	return this->lexema;
+}
--- JAMP laba/src/LEXICA/staff/Token.h
+++ JAMP laba/src/LEXICA/staff/Token.h
@@ -1,5 +1,25 @@
 #pragma once
+#include <iostream>
+#include <string>
 class Token
 {
-};
+public:
+	enum class token_type
+	{
+		key_word,
+		function,
+		variant_,
+		opErator,
+		constant,
+		divider_,
+	};
+private:
+	token_type type;
+	std::string lexema;
+public:
+	Token(const token_type& type, const std::string& lexema);
+
+	const token_type& get_type();
+	const std::string& get_lexema();
+};	
 
